5ed5b11a5193f0cc34cbe8c309cdac6763af82e3
minhnt2018 <minhnt@skcompany.net>
2020-05-05 23:07:44 +0700

    Update: -master Section link with Department - master Department link with Section - change type field Closing Date

--- custom/Maintain_Custom_Department/models/custom_department_model.py
+++ custom/Maintain_Custom_Department/models/custom_department_model.py
@@ -7,3 +7,4 @@ class ClassDepartmentCustom(models.Model):
 
     department_code = fields.Char('Department Code')
     parent_department_code = fields.Many2one( 'company.office.custom','Parent Department Code')
+    section_id = fields.One2many('section.model', 'department_code1', 'Section', copy=True)
--- custom/Maintain_Custom_Department_Section/models/custom_department_section_model.py
+++ custom/Maintain_Custom_Department_Section/models/custom_department_section_model.py
@@ -1,14 +1,39 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
-class ClassDepartmentCustom(models.Model):
+class ClassDepartmentSectionCustom(models.Model):
     _name = 'section.model'
 
-    section_name = fields.Char('Section Name')
+    name = fields.Char('Section Name')
     department_section_code = fields.Char('Section Code')
     parent_department_code = fields.Char('Parent Department Code')
     department_code1 = fields.Many2one('hr.department')
+    department_fake_id = fields.Char('id')
 
+    @api.model
+    def create(self, values):
 
+        department = self.env['hr.department'].create({
+            'name': values['name'],
+            'parent_id': values['department_code1'],
+            'department_code': values['department_section_code']
+        })
+        values['department_fake_id'] = department.id
 
+        section = super(ClassDepartmentSectionCustom, self).create(values)
+
+        return section
+
+    # def write(self, vals):
+    #
+    #     department = self.env['hr.department'].create({
+    #         'name': vals['name'],
+    #         'parent_id': vals['department_code1'],
+    #         'department_code': vals['department_section_code']
+    #     })
+    #     vals['department_fake_id'] = department.id
+    #
+    #     section = super(ClassDepartmentSectionCustom, self).write(vals)
+    #     return section
