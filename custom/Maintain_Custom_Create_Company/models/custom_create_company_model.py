5a5217d10f2e76b47c9e9bb9958abd17ff437893
minhnt2018 <minhnt@skcompany.net>
2020-05-05 14:28:06 +0700

    Update field vs translate Create Company(自社マスタ設定)

--- custom/Maintain_Custom_Create_Company/models/custom_create_company_model.py
+++ custom/Maintain_Custom_Create_Company/models/custom_create_company_model.py
@@ -7,4 +7,15 @@ class ClassCreateCompany(models.Model):
     _inherit = ['res.company']
 
     company_payment_term = fields.Many2one('account.payment.term', string='Payment Term')
+    company_fax = fields.Char('Fax')
+    company_closing_date = fields.Date('Closing Date')
+    company_bank_name_1 = fields.Char('Bank Name 1')
+    company_bank_name_2 = fields.Char('Bank Name 2')
+    company_bank_name_3 = fields.Char('Bank Name 3')
+    company_bank_name_4 = fields.Char('Bank Name 4')
+    company_bank_account_1 = fields.Integer('Bank Account 1')
+    company_bank_account_2 = fields.Integer('Bank Account 2')
+    company_bank_account_3 = fields.Integer('Bank Account 3')
+    company_bank_account_4 = fields.Integer('Bank Account 4')
+
 
