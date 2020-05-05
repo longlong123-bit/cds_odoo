13755f800fc68fc77730d8df58c2242f52296c20
minhnt2018 <minhnt@skcompany.net>
2020-05-05 11:22:08 +0700

    Update: - Fields for master Custom Employee(担当者マスタ) - Translate Japan

--- custom/Maintain_Custom_Employee/models/custom_employee_model.py
+++ custom/Maintain_Custom_Employee/models/custom_employee_model.py
@@ -6,6 +6,8 @@ class ClassEmployeeCusstom(models.Model):
     _inherit = ["hr.employee"]
 
     employee_code = fields.Char('Employee Code')
+    employee_password = fields.Char('Password')
+    employee_section_id = fields.Many2one('section.model', string='Section Code')
 
 
 
