53821a71646d325d58bedbbaff9f3dbcad37a03e
minhnt2018 <minhnt@skcompany.net>
2020-05-05 10:41:58 +0700

    Revert "Update field for master  Section(課マスタ) - Add translate Japan"
    This reverts commit f6fe94928cbd6719475cc2d597585ae48e805bf3.

---
+++ custom/Maintain_Custom_Department_Section/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Section',
+    'version': '13.0',
+    'author': 'BinhDT',
+    'license': 'AGPL-3',
+    'category': 'Extra Tools',
+    'description': """
+""",
+    'depends': [
+        'base_setup', 'mail', 'hr'
+    ],
+    'website': '',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/add_js.xml',
+        'views/custom_department_sector_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
+
