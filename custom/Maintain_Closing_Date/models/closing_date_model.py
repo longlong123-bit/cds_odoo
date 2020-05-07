2be1637cca3cc2ef74a926c4255178113dd99c05
minhnt2018 <minhnt@skcompany.net>
2020-05-07 18:08:07 +0700

    Update: add validate code in master Closing Date - add field in tree view - add field in search filter - add translate

--- custom/Maintain_Closing_Date/models/closing_date_model.py
+++ custom/Maintain_Closing_Date/models/closing_date_model.py
@@ -1,15 +1,34 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
-class ClassBillSchema(models.Model):
+class ClassClosingDate(models.Model):
     _name = 'closing.date'
 
     closing_date_code = fields.Char('Closing Date Code')
-    closing_date_name = fields.Char('Closing Date Name')
+    name = fields.Char('Closing Date Name')
     start_day = fields.Integer('Start Date')
     closing_date_circle = fields.Integer('Closing Date Circle')
     active = fields.Boolean('Active', default=True)
+
+    _sql_constraints = [
+        ('name_code_uniq', 'unique(closing_date_code)', 'The code must be unique!')
+    ]
+
+    @api.constrains('closing_date_code')
+    def _check_unique_searchkey(self):
+        exists = self.env['closing.date'].search(
+            [('closing_date_code', '=', self.closing_date_code), ('id', '!=', self.id)])
+        if exists:
+            raise ValidationError(_('The code must be unique!'))
+
+    def copy(self, default=None):
+        default = dict(default or {})
+        default.update({'closing_date_code': ''})
+        if 'name' not in default:
+            default['name'] = _("%s (copy)") % (self.name)
+        return super(ClassClosingDate, self).copy(default)
 
 
 
