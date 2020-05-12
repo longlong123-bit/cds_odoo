5cca3be6c26f9e7b44596f075df6ab9e7a49a84a
minhnt2018 <minhnt@skcompany.net>
2020-05-12 14:40:37 +0700

    Update tree list detail in master Partner Remake

--- custom/Maintain_Business_Partners_Remake/models/partner_customer_remake.py
+++ custom/Maintain_Business_Partners_Remake/models/partner_customer_remake.py
@@ -13,6 +13,9 @@ class NewClassPartnerCustom(models.Model):
     # _name = 'partner.customer.custom'
     _inherit = 'res.partner'
 
+    # relation customer
+    relation_id = fields.One2many('relation.partner.model', 'partner_id', string='Relation')
+    # name
     name = fields.Char(string='Customer Name')
     customer_code = fields.Char(string='Customer Code')
     # 請求先コード
@@ -25,7 +28,7 @@ class NewClassPartnerCustom(models.Model):
     customer_fax = fields.Char('Fax')
     customer_phone = fields.Char('Phone')
     customer_state = fields.Many2one('res.country.state', string='State')
-    customer_supplier_group_code = fields.Char('Supplier Group Code')
+    customer_supplier_group_code = fields.Many2one('business.partner.group.custom','Supplier Group Code')
     customer_industry_code = fields.Many2one('res.partner.industry', string='Industry Code')
     # 担当者
     customer_agent = fields.Many2one('res.users', string='Representative/Agent')
@@ -45,6 +48,10 @@ class NewClassPartnerCustom(models.Model):
     customer_payment_method = fields.Selection([('normal', 'Normal'), ('deposit', 'Deposit')], 'Payment Method')
     # 回収方法
     customer_collect_method = fields.Char('Collect Method')
+    payment_rule = fields.Selection(
+        [('rule_cash', 'Cash'),('rule_check', 'Check'),('rule_credit','Credit Card'),('rule_direct_debit','Direct Debit'),
+         ('rule_deposit','Direct Deposit'),('rule_on_credit','On Credit')], 'Collect Method', store=True,
+         )
     # 回収月、回収日
     customer_collect_circle = fields.Char('Collect Circle')
     # 掛率設定
@@ -52,7 +59,7 @@ class NewClassPartnerCustom(models.Model):
     # 掛率
     customer_rate = fields.Integer('Hang Rate')
     # 請求値引区分
-    customer_bill_discount = fields.Char('Bill Discount')
+    customer_bill_discount = fields.Boolean('Bill Discount')
     # 請求値引率
     customer_bill_discount_rate = fields.Char('Bill Discount Rate')
     # 抜粋請求区分
@@ -92,3 +99,38 @@ class NewClassPartnerCustom(models.Model):
         if 'name' not in default:
             default['name'] = _("%s (copy)") % (self.name)
         return super(NewClassPartnerCustom, self).copy(default)
+
+    # Relation Partner Class
+class ClassRelationPartnerCustom(models.Model):
+    _name = 'relation.partner.model'
+
+    partner_id = fields.Many2one('res.partner', ondelete='cascade',
+                                 required=True, index=True)
+    relate_customer_organization = fields.Many2one('res.company', string='Organization')
+    name = fields.Char('Name')
+    relate_business_partner = fields.Many2one('res.partner', string='Business Partner')
+    relate_partner_location = fields.Char('Partner Location')
+    relate_related_partner = fields.Many2one('res.partner', string='Related Partner')
+    relate_related_partner_location = fields.Char('Related Partner Location')
+    relate_ship_address = fields.Boolean('Ship Address')
+    relate_invoic_address = fields.Boolean('Invoice Address')
+    relate_payfrom_address = fields.Boolean('Pay-From Address')
+    relate_remitto_address = fields.Boolean('Remit-To Address')
+    active = fields.Boolean('Active', default=True)
+
+    def button_details(self):
+        view = {
+            'name': _('Detail'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'relation.partner.model',
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+            'readonly': True,
+            'res_id': self.id,
+        }
+        return view
+
+    def action_close_dialog(self):
+        return {'type': 'ir.actions.act_window_close'}
