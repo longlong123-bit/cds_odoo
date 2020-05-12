4e7f61ca671f1503e83cc7f6e384a9236d1a3079
minhnt2018 <minhnt@skcompany.net>
2020-05-12 14:00:29 +0700

    Update unique code for Business Partner Group

--- custom/Maintain_Business_Partner_Group/models/partner_group.py
+++ custom/Maintain_Business_Partner_Group/models/partner_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class ClassPartnerGroup(models.Model):
@@ -9,5 +10,23 @@ class ClassPartnerGroup(models.Model):
     name = fields.Char('name', required=True)
     description =fields.Char('description')
     active = fields.Boolean('isactive', default=True)
+
+    _sql_constraints = [
+        ('name_code_uniq', 'unique(partner_group_code)', 'The code must be unique!')
+    ]
+
+    @api.constrains('partner_group_code')
+    def _check_unique_searchkey(self):
+        exists = self.env['business.partner.group.custom'].search(
+            [('partner_group_code', '=', self.partner_group_code), ('id', '!=', self.id)])
+        if exists:
+            raise ValidationError(_('The code must be unique!'))
+
+    def copy(self, default=None):
+        default = dict(default or {})
+        default.update({'partner_group_code': ''})
+        if 'name' not in default:
+            default['name'] = _("%s (copy)") % (self.name)
+        return super(ClassPartnerGroup, self).copy(default)
 
 
