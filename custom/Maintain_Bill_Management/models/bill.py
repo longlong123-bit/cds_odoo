e9ece2091b7dcc1ccaf33fc3ad4ad0f9393f27ca
Tran Lam Toi <toitl@skcompany.net>
2020-05-14 16:51:55 +0700

    Update view for bill

--- custom/Maintain_Bill_Management/models/bill.py
+++ custom/Maintain_Bill_Management/models/bill.py
@@ -32,3 +32,27 @@ class BmBill(models.Model):
     office_code = fields.Char(compute=_get_office_code, readonly=True, store=False)
     office_name = fields.Char(compute=_get_office_name, readonly=True, store=False)
     office_closing_date = fields.Date(compute=_get_office_closing_date, readonly=True, store=False)
+
+    # Cancel bill
+    # When user click on button Cancel in tree
+    def cancel_bill(self):
+        # Get bill
+        bills = self.browse(self._ids)
+
+        if bills:
+            for bill in bills:
+                # Set status to cancel for bill
+                bill.write({
+                    'status': 'cancel'
+                })
+
+                # Unlock all invoices of bill
+                for voucher in bill.bill_vouchers:
+                    voucher.invoice_id.write({
+                        'is_billed': False
+                    })
+
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload'
+        }
