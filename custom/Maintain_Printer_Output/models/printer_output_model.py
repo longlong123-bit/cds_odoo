fbae189026c60ef4b9a7be7ef9b44f0dfd1fdb27
minhnt2018 <minhnt@skcompany.net>
2020-05-26 10:27:30 +0700

    Printer Output: Change name Field Customer master: Add hide field in tree

--- custom/Maintain_Printer_Output/models/printer_output_model.py
+++ custom/Maintain_Printer_Output/models/printer_output_model.py
@@ -10,16 +10,16 @@ class ClassPrinterOutput(models.Model):
     paperformat_id_2 = fields.Selection([('all', 'All'),('quotation', 'Quotation'),('voucher', 'Voucher'),('invoice','Invoice'),('report','Report')], 'Paper Format 2')
     paperformat_id_3 = fields.Selection([('all', 'All'),('quotation', 'Quotation'),('voucher', 'Voucher'),('invoice','Invoice'),('report','Report')], 'Paper Format 3')
     paperformat_id_4 = fields.Selection([('all', 'All'),('quotation', 'Quotation'),('voucher', 'Voucher'),('invoice','Invoice'),('report','Report')], 'Paper Format 4')
-    printer_output_1 = fields.Selection([('printer1', 'Printer 1'),('printer2', 'Printer 2'),('printer3','Printer 3'),('printer4','Printer 4')],'Printer Output')
+    printer_output_1 = fields.Selection([('printer1', 'Printer 1'),('printer2', 'Printer 2'),('printer3','Printer 3'),('printer4','Printer 4')],'Printer Output 1')
     printer_output_2 = fields.Selection(
         [('printer1', 'Printer 1'), ('printer2', 'Printer 2'), ('printer3', 'Printer 3'), ('printer4', 'Printer 4')],
-        'Printer Output')
+        'Printer Output 2')
     printer_output_3 = fields.Selection(
         [('printer1', 'Printer 1'), ('printer2', 'Printer 2'), ('printer3', 'Printer 3'), ('printer4', 'Printer 4')],
-        'Printer Output')
+        'Printer Output 3')
     printer_output_4 = fields.Selection(
         [('printer1', 'Printer 1'), ('printer2', 'Printer 2'), ('printer3', 'Printer 3'), ('printer4', 'Printer 4')],
-        'Printer Output')
+        'Printer Output 4')
     active = fields.Boolean('Active', default=True)
 
 
