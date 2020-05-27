5467b1271e05ecfa76c5bdbb5a7b761814b68499
Tran Lam Toi <toitl@skcompany.net>
2020-05-27 17:18:49 +0700

    Update invoice lines

--- custom/Maintain_Custom_Common/static/src/js/show_lines.js
+++ custom/Maintain_Custom_Common/static/src/js/show_lines.js
@@ -11,48 +11,9 @@ odoo.define('Maintain_Custom_Common.show_lines', function(require){
         /**
          * Render line
          */
-        _renderLines: function($target, data){
-            var $table = null;
-
-            if (data.template) {
-                $table = QWeb.render(data.template, {records: data.records});
-            } else {
-                // Table of line
-                $table = $('<table></table');
-                var $tbody = $('<tbody></tbody>');
-                var $thead = $('<thead></thead>');
-                var $trHead = $('<tr></tr>');
-                $thead.append($trHead);
-                $table.attr({
-                    'class': 'table-lines'
-                });
-
-                // Get keys and generate header
-                var headers = Object.keys(data[0]);
-
-                for (var i = 0; i < headers.length; i++) {
-                    $trHead.append('<th>' + headers[i] + '</th>');
-                }
-
-                // Add line to table of line
-                for (var i = 0; i < data.length; i++) {
-                    var $ltr = $('<tr></tr>');
-
-                    for (var j = 0; j < headers.length; j++) {
-                        if (data[i][headers[j]] !== undefined) {
-                            $ltr.append('<td>' + data[i][headers[j]] + '</td>');
-                        }
-                    }
-
-                    $tbody.append($ltr);
-                }
-
-                // Add table of line to row
-                $table.append($thead);
-                $table.append($tbody);
-            }
-
-            $target.append($table);
+        _renderTemplate: function($target, data){
+            var html = QWeb.render(data.template, {records: data.records});
+            $target.append(html);
         },
 
         /**
@@ -60,29 +21,33 @@ odoo.define('Maintain_Custom_Common.show_lines', function(require){
          */
         _renderRowOfLines: function(id, data){
             var $target = $('tr[data-id="'+id+'"]');
+
             // Create new row
-            var $tr = $('<tr></tr>');
-            $tr.attr({
-                'data-ref': $target.attr('data-id'),
-                'class': 'row-lines',
-                'style': 'display: none'
-            });
+            var $tr = $('tr.row-lines[data-ref="'+ id +'"]');
+
+            if ($tr.length === 0) {
+                $tr = $('<tr></tr>');
+                $tr.attr({
+                    'data-ref': $target.attr('data-id'),
+                    'class': 'row-lines'
+                });
+            } else {
+                $tr.removeClass('empty').empty();
+            }
+
+            $tr.css({display: 'none'});
 
             if (data.records.length > 0) {
                 $tr.append('<td></td>');
 
                 // Create column contains table lines
                 var $td = $('<td></td>');
-                var $divTag = $('<div></div>');
-                $divTag.addClass('table-responsive');
-                $td.append($divTag);
                 $td.attr({
                     colspan: $target.find('td').length - 1
                 });
                 $tr.append($td);
 
-                this._renderLines($divTag, data);
-
+                this._renderTemplate($td, data);
             } else {
                 $tr.addClass('empty');
             }
@@ -116,12 +81,14 @@ odoo.define('Maintain_Custom_Common.show_lines', function(require){
         _onButtonClicked: function($ev){
             if ($ev.data.attrs.class === 'o_button_line') {
                 // Check if row is exists
-                var checkRow = $('tr.row-lines[data-ref="'+$ev.data.record.id+'"]');
+                var $checkRow = $('tr.row-lines[data-ref="'+$ev.data.record.id+'"]');
                 var self = this;
 
-                if (checkRow.length > 0) {
-                    this._toggleRow($ev.data.record.id);
-                    return;
+                if ($checkRow.length > 0) {
+                    if ($checkRow.css('display') != 'none') {
+                        this._toggleRow($ev.data.record.id);
+                        return;
+                    }
                 }
 
                 // Call to server
