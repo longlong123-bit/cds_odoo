997cddd778b0d18a5911d11ee0f307fcf31756b0
minhnt2018 <minhnt@skcompany.net>
2020-05-28 13:40:51 +0700

    Invoice Master: Fix Tree detail Add translate

--- custom/Maintain_Invoice_Remake/models/invoice_customer_custom.py
+++ custom/Maintain_Invoice_Remake/models/invoice_customer_custom.py
@@ -758,6 +758,38 @@ class ClassInvoiceCustom(models.Model):
             'records': records
         }
 
+    def _check_balanced(self):
+        ''' Assert the move is fully balanced debit = credit.
+        An error is raised if it's not the case.
+        '''
+        moves = self.filtered(lambda move: move.line_ids)
+        if not moves:
+            return
+
+        # /!\ As this method is called in create / write, we can't make the assumption the computed stored fields
+        # are already done. Then, this query MUST NOT depend of computed stored fields (e.g. balance).
+        # It happens as the ORM makes the create with the 'no_recompute' statement.
+        self.env['account.move.line'].flush(['debit', 'credit', 'move_id'])
+        self.env['account.move'].flush(['journal_id'])
+        self._cr.execute('''
+            SELECT line.move_id, ROUND(SUM(debit - credit), currency.decimal_places)
+            FROM account_move_line line
+            JOIN account_move move ON move.id = line.move_id
+            JOIN account_journal journal ON journal.id = move.journal_id
+            JOIN res_company company ON company.id = journal.company_id
+            JOIN res_currency currency ON currency.id = company.currency_id
+            WHERE line.move_id IN %s
+            GROUP BY line.move_id, currency.decimal_places
+            HAVING ROUND(SUM(debit - credit), currency.decimal_places) != 0.0;
+        ''', [tuple(self.ids)])
+
+        query_res = self._cr.fetchall()
+        if query_res:
+            return
+            # ids = [res[0] for res in query_res]
+            # sums = [res[1] for res in query_res]
+            # raise UserError(_("Cannot create unbalanced journal entry. Ids: %s\nDifferences debit - credit: %s") % (ids, sums))
+
 class AccountTaxLine(models.Model):
     _name = 'account.tax.line'
     move_name = fields.Char('Invoice')
