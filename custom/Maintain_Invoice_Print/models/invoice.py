e9fac8ed6daedc9babb1f66190022c1c1cc10c64
Tran Lam Toi <toitl@skcompany.net>
2020-05-31 17:04:31 +0700

    Update report

--- custom/Maintain_Invoice_Print/models/invoice.py
+++ custom/Maintain_Invoice_Print/models/invoice.py
@@ -3,16 +3,6 @@ from odoo import api, fields, models
 class PrintInvoice(models.Model):
     _inherit = 'account.move'
 
-    def _get_display_invoiced_date(self):
-        for rec in self:
-            rec.invoiced_date_year = rec.x_studio_date_invoiced.year
-            rec.invoiced_date_month = rec.x_studio_date_invoiced.month
-            rec.invoiced_date_day = rec.x_studio_date_invoiced.day
-
-    invoiced_date_year = fields.Char(readonly=True, store=False, compute=_get_display_invoiced_date)
-    invoiced_date_month = fields.Char(readonly=True, store=False)
-    invoiced_date_day = fields.Char(readonly=True, store=False)
-
     def report_dialog(self):
         return {
             'type': ''
