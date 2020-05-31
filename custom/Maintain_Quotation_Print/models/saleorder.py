b03fd4987120df551ba5b7b58bb285a48b867ad1
Tran Lam Toi <toitl@skcompany.net>
2020-05-31 17:03:22 +0700

    Update report

--- custom/Maintain_Quotation_Print/models/saleorder.py
+++ custom/Maintain_Quotation_Print/models/saleorder.py
@@ -3,16 +3,6 @@ from odoo import models, fields
 class PrintSale(models.Model):
     _inherit = 'sale.order'
 
-    def _get_display_expected_date(self):
-        for rec in self:
-            rec.expected_date_year = rec.expected_date.year
-            rec.expected_date_month = rec.expected_date.month
-            rec.expected_date_day = rec.expected_date.day
-
-    expected_date_year = fields.Char(readonly=True, store=False, compute=_get_display_expected_date)
-    expected_date_month = fields.Char(readonly=True, store=False)
-    expected_date_day = fields.Char(readonly=True, store=False)
-
     # Preview report
     def preview_report(self):
         return {
