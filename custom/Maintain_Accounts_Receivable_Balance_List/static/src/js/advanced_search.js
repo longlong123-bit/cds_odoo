3566e019f28594d4ebb1e9dc2f9a11b3010cf3e0
nghiant <nghiant@skcompany.net>
2020-06-11 14:31:34 +0700

    Handle checked all check box when init screen

--- custom/Maintain_Accounts_Receivable_Balance_List/static/src/js/advanced_search.js
+++ custom/Maintain_Accounts_Receivable_Balance_List/static/src/js/advanced_search.js
@@ -1,6 +1,7 @@
 odoo.define('Accounts_Receivable_Balance_List.Search', function(require){
     var FilterMenu = require('web.FilterMenu_free');
 
+    // Filter menu in advanced search
     FilterMenu.include({
         advancedSearch: _.extend({}, FilterMenu.prototype.advancedSearch || {}, {
             'res.partner': {
@@ -8,4 +9,21 @@ odoo.define('Accounts_Receivable_Balance_List.Search', function(require){
             }
         })
     });
+
+    // Handle checked all check box when init screen
+    intervalCheckBox = setInterval(handleCheckAllCheckBox, 1000);
+    function handleCheckAllCheckBox() {
+        content = document.body.getElementsByClassName('o_content');
+        if (content.length > 0) {
+            check_box_all = $('.o_list_view th.o_list_record_selector input[type=checkbox]');
+            if (check_box_all.length > 0) {
+                check_box_all_id = check_box_all[0].id;
+                if (check_box_all_id) {
+                    $('#' + check_box_all_id).trigger('click');
+                    $('.o_list_record_selector').css('pointer-events', 'none');
+                    clearInterval(intervalCheckBox);
+                }
+            }
+        }
+    };
 });
