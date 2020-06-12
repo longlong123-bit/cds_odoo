96880a8ae8f6ea30f2d151d97816c59ab353bd52
Le Hoang Han <hoanghan.le.95@gmail.com>
2020-06-12 10:01:09 +0700

    Comment function handleCheckAllCheckBox()

--- custom/Maintain_Accounts_Receivable_Balance_List/static/src/js/advanced_search.js
+++ custom/Maintain_Accounts_Receivable_Balance_List/static/src/js/advanced_search.js
@@ -11,20 +11,20 @@ odoo.define('Accounts_Receivable_Balance_List.Search', function(require){
     });
 
     // Handle checked all check box when init screen
-    intervalCheckBox = setInterval(handleCheckAllCheckBox, 1000);
-    function handleCheckAllCheckBox() {
-        content = document.body.getElementsByClassName('o_content');
-        if (content.length > 0) {
-            check_box_all = $('.o_list_view th.o_list_record_selector input[type=checkbox]');
-            if (check_box_all.length > 0) {
-                check_box_all_id = check_box_all[0].id;
-                if (check_box_all_id) {
-                    if ($('#' + check_box_all_id).prop('checked') == false) {
-                        $('#' + check_box_all_id).trigger('click');
-                        $('.o_list_record_selector').css('pointer-events', 'none');
-                    }
-                }
-            }
-        }
-    };
+    // intervalCheckBox = setInterval(handleCheckAllCheckBox, 1000);
+    // function handleCheckAllCheckBox() {
+    //     content = document.body.getElementsByClassName('o_content');
+    //     if (content.length > 0) {
+    //         check_box_all = $('.o_list_view th.o_list_record_selector input[type=checkbox]');
+    //         if (check_box_all.length > 0) {
+    //             check_box_all_id = check_box_all[0].id;
+    //             if (check_box_all_id) {
+    //                 if ($('#' + check_box_all_id).prop('checked') == false) {
+    //                     $('#' + check_box_all_id).trigger('click');
+    //                     $('.o_list_record_selector').css('pointer-events', 'none');
+    //                 }
+    //             }
+    //         }
+    //     }
+    // };
 });
