4b0253d7682da866a83669a0195af8a0420ac76e
tai <xuantai021997@gmail.com>
2020-06-19 00:32:56 +0700

    Commit Bill List Module

---
+++ custom/Maintain_Bill_List/static/src/js/bill_list_advanced_search.js
@@ -0,0 +1,11 @@
+odoo.define('bill_list.Advanced_Search', function(require){
+    var FilterMenu = require('web.FilterMenu_free');
+    var advancedSearch = _.extend({}, FilterMenu.prototype.advancedSearch || {});
+    advancedSearch['bill.info'] = advancedSearch['bill.info'] || {};
+    advancedSearch['bill.info']['bm.bill.list.tree'] = {
+        template: 'bill_list.advanced_search'
+    }
+    FilterMenu.include({
+        advancedSearch: advancedSearch
+    });
+});
