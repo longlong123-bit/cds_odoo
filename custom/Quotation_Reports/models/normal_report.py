4142f82b1f4e9c63f0ea1fd3011b91ef25909742
tai <xuantai021997@gmail.com>
2020-06-26 21:10:18 +0700

    update models

--- custom/Quotation_Reports/models/normal_report.py
+++ custom/Quotation_Reports/models/normal_report.py
@@ -1,16 +1,4 @@
 from odoo import models, fields
-from datetime import datetime
-
-# from erajp.converter import strjpftime
-
-
-ERA_JP = (
-    ("M", "明治"),
-    ("T", "大正"),
-    ("S", "昭和"),
-    ("H", "平成"),
-    ("R", "令和"),
-)
 
 
 class PrintSale(models.Model):
