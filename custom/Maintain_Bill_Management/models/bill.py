b63af2260411aeaafa4f1c6c061dc8e62088b4b8
Le Hoang Han <hoanghan.le.95@gmail.com>
2020-07-14 14:56:24 +0700

    Update source

--- custom/Maintain_Bill_Management/models/bill.py
+++ custom/Maintain_Bill_Management/models/bill.py
@@ -697,4 +697,3 @@ class InvoiceLineClass(models.Model):
 
     customer_code = fields.Char(compute=compute_data, string='Customer Code', store=False)
     customer_name = fields.Char(compute=compute_data, string='Customer Name', store=False)
-o
