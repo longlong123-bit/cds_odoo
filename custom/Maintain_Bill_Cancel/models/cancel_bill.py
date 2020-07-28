29a43378b25c5d8556c45252df0ff656e7c78c3f
Le Hoang Han <hoanghan.le.95@gmail.com>
2020-07-28 23:26:47 +0700

    [Fix] update seikyuu

--- custom/Maintain_Bill_Cancel/models/cancel_bill.py
+++ custom/Maintain_Bill_Cancel/models/cancel_bill.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models
 
 
 class BillingClass(models.Model):
@@ -6,78 +6,69 @@ class BillingClass(models.Model):
 
     def cancel_bill_for_invoice(self, argsSelectedData, argsSelectedIds):
         for rec in argsSelectedData:
-            res_partner_id = self.env["res.partner"].search(
-                ['|', ('customer_code', '=', rec['billing_code']), ('customer_code_bill', '=', rec['billing_code'])])
+            bill_invoice_domain = [
+                ('bill_info_id', '=', rec['id']),
+            ]
+            bill_invoice_details_domain = [
+                ('bill_info_id', '=', rec['id']),
+            ]
 
-            if rec['last_closing_date']:
-                invoice_ids = self.env['account.move'].search([
+            bill_info_lasted_domain = [
+                ('billing_code', '=', rec['billing_code']),
+            ]
+            bill_info_lasted = self.env['bill.info'].search(bill_info_lasted_domain,
+                                                            order='deadline desc, create_date desc', limit=1)
+
+            if rec['id'] == bill_info_lasted.id:
+                bill_info_ids = self.env['bill.info'].browse(argsSelectedIds)
+                bill_invoice_ids = self.env['bill.invoice'].search(bill_invoice_domain)
+                bill_invoice_details_ids = self.env['bill.invoice.details'].search(bill_invoice_details_domain)
+
+                res_partner_id = self.env["res.partner"].search(
+                    ['|', ('customer_code', '=', rec['billing_code']),
+                     ('customer_code_bill', '=', rec['billing_code'])])
+
+                invoice_ids_domain = [
                     ('partner_id', 'in', res_partner_id.ids),
-                    ('x_studio_date_invoiced', '>', rec['last_closing_date']),
                     ('x_studio_date_invoiced', '<=', rec['deadline']),
                     ('state', '=', 'posted'),
                     ('type', '=', 'out_invoice'),
                     ('bill_status', '=', 'billed'),
-                ])
-                payment_ids = self.env['account.payment'].search([
-                    ('partner_id', 'in', res_partner_id.ids),
-                    ('payment_date', '>', rec['last_closing_date']),
-                    ('payment_date', '<=', rec['deadline']),
-                    ('state', '=', 'posted'),
-                    ('bill_status', '=', 'billed'),
-                ])
-            else:
-                invoice_ids = self.env['account.move'].search([
-                    ('partner_id', 'in', res_partner_id.ids),
-                    ('x_studio_date_invoiced', '<=', rec['deadline']),
-                    ('state', '=', 'posted'),
-                    ('type', '=', 'out_invoice'),
-                    ('bill_status', '=', 'billed'),
-                ])
-                payment_ids = self.env['account.payment'].search([
+                ]
+
+                payment_ids_domain = [
                     ('partner_id', 'in', res_partner_id.ids),
                     ('payment_date', '<=', rec['deadline']),
                     ('state', '=', 'posted'),
                     ('bill_status', '=', 'billed'),
-                ])
+                ]
+                if rec['last_closing_date']:
+                    invoice_ids_domain += [('x_studio_date_invoiced', '>', rec['last_closing_date'])]
+                    payment_ids_domain += [('payment_date', '>', rec['last_closing_date'])]
 
-            invoice_ids.write({
-                'bill_status': 'not yet'
-            })
-            self.env['account.move.line'].search([('move_id', 'in', invoice_ids.ids)]).write({
-                'bill_status': 'not yet',
-                'selected': False
-            })
+                invoice_ids = self.env['account.move'].search(invoice_ids_domain)
+                payment_ids = self.env['account.payment'].search(payment_ids_domain)
+                invoice_ids.write({
+                    'bill_status': 'not yet'
+                })
+                self.env['account.move.line'].search([
+                    ('id', 'in', bill_invoice_details_ids.account_move_line_id.ids)
+                ]).write({
+                    'bill_status': 'not yet',
+                })
 
-            payment_ids.write({
-                'bill_status': 'not yet'
-            })
-            payment_ids.cancel()
-            payment_ids.action_draft()
+                payment_ids.write({
+                    'bill_status': 'not yet'
+                })
+                payment_ids.cancel()
+                payment_ids.action_draft()
 
-            self.env['bill.info'].browse(argsSelectedIds).unlink()
-
-            if rec['last_closing_date']:
-                self.env['bill.invoice'].search([
-                    ('billing_code', '=', rec['billing_code']),
-                    ('bill_date', '=', rec['bill_date']),
-                    ('last_closing_date', '=', rec['last_closing_date']),
-                ]).unlink()
-
-                self.env['bill.invoice.details'].search([
-                    ('billing_code', '=', rec['billing_code']),
-                    ('bill_date', '=', rec['bill_date']),
-                    ('last_closing_date', '=', rec['last_closing_date']),
-                ]).unlink()
+                bill_info_ids.unlink()
+                bill_invoice_ids.unlink()
+                bill_invoice_details_ids.unlink()
             else:
-                self.env['bill.invoice'].search([
-                    ('billing_code', '=', rec['billing_code']),
-                    ('bill_date', '=', rec['bill_date']),
-                ]).unlink()
+                return False
 
-                self.env['bill.invoice.details'].search([
-                    ('billing_code', '=', rec['billing_code']),
-                    ('bill_date', '=', rec['bill_date']),
-                ]).unlink()
         if not argsSelectedData:
             return False
         else:
@@ -85,4 +76,3 @@ class BillingClass(models.Model):
                 'type': 'ir.actions.client',
                 'tag': 'reload',
             }
-
