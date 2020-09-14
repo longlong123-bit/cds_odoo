f29f4796c52131209408c074aa6e5cfe05b7585f
taideptroai <xuantai021997@gmail.com>
2020-09-14 15:11:56 +0700

    [FIX]Quotation_Reports: limit character in quotation reports 1

--- custom/Quotation_Reports/models/normal_report.py
+++ custom/Quotation_Reports/models/normal_report.py
@@ -1,4 +1,6 @@
 from odoo import models, fields
+import jaconv
+
 
 
 class PrintSale(models.Model):
@@ -12,3 +14,80 @@ class PrintSale(models.Model):
             'model': 'sale.order',
             'report_type': "qweb-html",
         }
+
+    def limit_chapter_reports(self, string_text=None):
+        string_text = jaconv.h2z(string_text, kana=True, digit=True, ascii=True)
+        count = 0
+        len_i = len(string_text)
+        byte_count = 0
+        while count < len_i and byte_count < 40:
+            byte_count += len(string_text[count].encode('SHIFT-JIS'))
+            count += 1
+        len_string = string_text[:count]
+        return len_string
+
+    def limit_number_field(self, number=0.00, number_len=20, name=False):
+        if name:
+            if number % 1 > 0:
+                number_len = number_len - 2
+                number = str(int(number))[:number_len] + str(number % 1)[1:]
+            else:
+                number = str(number)[:number_len]
+        else:
+            if len(str(number)) > number_len:
+                number = str(number)[:number_len]
+        return float(number)
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    def limit_charater_field(self, string_text=None, text_len=20, name=False, first1=True):
+        len_string = ''
+        string_text = jaconv.h2z(string_text, kana=True, digit=True, ascii=True)
+        if name:
+            string_text1 = ''
+            string_text2 = ''
+            if "\n" in string_text:
+                string_text1 = string_text.splitlines()[0]
+                string_text2 = string_text.splitlines()[1]
+            else:
+                string_text1 = string_text
+                string_text2 = ''
+            if first1:
+                count = 0
+                len_i = len(string_text1)
+                byte_count = 0
+                while count < len_i and byte_count < text_len:
+                    byte_count += len(string_text1[count].encode('SHIFT-JIS'))
+                    count += 1
+                len_string = string_text1[:count]
+            else:
+                count = 0
+                len_i = len(string_text2)
+                byte_count = 0
+                while count < len_i and byte_count < text_len:
+                    byte_count += len(string_text2[count].encode('SHIFT-JIS'))
+                    count += 1
+                len_string = string_text2[:count]
+        else:
+            count = 0
+            len_i = len(string_text)
+            byte_count = 0
+            while count < len_i and byte_count < text_len:
+                byte_count += len(string_text[count].encode('SHIFT-JIS'))
+                count += 1
+            len_string = string_text[:count]
+        return len_string
+
+    def limit_number_field(self, number=0.00, number_len=20, name=False):
+        if name:
+            if number % 1 > 0:
+                number_len = number_len - 2
+                number = str(int(number))[:number_len] + str(number % 1)[1:]
+            else:
+                number = str(number)[:number_len]
+        else:
+            if len(str(number)) > number_len:
+                number = str(number)[:number_len]
+        return float(number)
