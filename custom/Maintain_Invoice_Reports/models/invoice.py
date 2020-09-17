8768af40102f0c5385e4bbaafe0d075cce3bbae5
taideptroai <xuantai021997@gmail.com>
2020-09-17 17:11:05 +0700

    [FIX]update limit character in invoice tmp

--- custom/Maintain_Invoice_Reports/models/invoice.py
+++ custom/Maintain_Invoice_Reports/models/invoice.py
@@ -1,5 +1,6 @@
 from odoo import models
 from custom.Maintain_Invoice_Remake.models.invoice_customer_custom import rounding
+import jaconv
 
 
 class InvoiceReports(models.Model):
@@ -34,3 +35,53 @@ class InvoiceReports(models.Model):
             if tax_rate == 0 and re.x_voucher_tax_transfer == 'custom_tax':
                 subtotal += re.x_voucher_tax_amount
         return rounding(subtotal, 0, self.partner_id.customer_tax_rounding)
+
+    def limit_charater_field(self, string_text=None, text_len=20, name=False, first1=True):
+        len_string = ''
+        string_text = jaconv.h2z(string_text, kana=True, digit=True, ascii=True).replace('\uff0d', '-').replace('\xa0', ' ').replace('\uff5e', '~')
+        if name:
+            string_text1 = ''
+            string_text2 = ''
+            if "\n" in string_text:
+                string_text1 = string_text.splitlines()[0]
+                string_text2 = string_text.splitlines()[1]
+            else:
+                string_text1 = string_text
+                string_text2 = ''
+            if first1:
+                count = 0
+                len_i = len(string_text1)
+                byte_count = 0
+                while count < len_i and byte_count < text_len:
+                    byte_count += len(string_text1[count])
+                    count += 1
+                len_string = string_text1[:count]
+            else:
+                count = 0
+                len_i = len(string_text2)
+                byte_count = 0
+                while count < len_i and byte_count < text_len:
+                    byte_count += len(string_text2[count])
+                    count += 1
+                len_string = string_text2[:count]
+        else:
+            count = 0
+            len_i = len(string_text)
+            byte_count = 0
+            while count < len_i and byte_count < text_len:
+                byte_count += len(string_text[count])
+                count += 1
+            len_string = string_text[:count]
+        return len_string.replace('-', '－').replace(' ', '　').replace('~', '～')
+
+    def limit_number_field(self, number=0.00, number_len=20, name=False):
+        if name:
+            if number % 1 > 0:
+                number_len = number_len - 2
+                number = str(int(number))[:number_len] + str(number % 1)[1:]
+            else:
+                number = str(number)[:number_len]
+        else:
+            if len(str(number)) > number_len:
+                number = str(number)[:number_len]
+        return float(number)
