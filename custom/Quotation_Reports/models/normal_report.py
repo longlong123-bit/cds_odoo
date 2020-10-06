7be71e166c22d3bf3b97a5d14b421530e313346a
taideptroai <xuantai021997@gmail.com>
2020-10-06 14:06:19 +0700

    [FIX]Quotation_Reports: update limmit character

--- custom/Quotation_Reports/models/normal_report.py
+++ custom/Quotation_Reports/models/normal_report.py
@@ -16,15 +16,18 @@ class PrintSale(models.Model):
         }
 
     def limit_chapter_reports(self, string_text=None):
-        string_text = jaconv.h2z(string_text, kana=True, digit=True, ascii=True).replace('\uff0d', '-').replace('\xa0', ' ').replace('\uff5e', '~')
+        # string_text = jaconv.h2z(string_text, kana=True, digit=True, ascii=True).replace('\uff0d', '-').replace('\xa0', ' ').replace('\uff5e', '~')
         count = 0
         len_i = len(string_text)
         byte_count = 0
-        while count < len_i and byte_count < 20:
-            byte_count += len(string_text[count])
+        while count < len_i and byte_count < 40:
+            if len(string_text[count].encode('utf-8')) > 1:
+                byte_count += 2
+            else:
+                byte_count += 1
             count += 1
         len_string = string_text[:count]
-        return len_string.replace('-', '－').replace(' ', '　').replace('~', '～')
+        return len_string
 
     def limit_number_field(self, number=0.00, number_len=20):
         if len(str(number)) > number_len:
@@ -36,9 +39,10 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     def limit_charater_field(self, string_text=None, text_len=20, name=False, first1=True):
+        text_len = text_len*2
         len_string = ''
         if string_text:
-            string_text = jaconv.h2z(string_text, kana=True, digit=True, ascii=True).replace('\uff0d', '-').replace('\xa0', ' ').replace('\uff5e', '~')
+            # string_text = jaconv.h2z(string_text, kana=True, digit=True, ascii=True).replace('\uff0d', '-').replace('\xa0', ' ').replace('\uff5e', '~')
             if name:
                 string_text1 = ''
                 string_text2 = ''
@@ -49,19 +53,42 @@ class SaleOrderLine(models.Model):
                     string_text1 = string_text
                     string_text2 = ''
                 if first1:
-                    len_string = string_text1[:text_len]
+                    count = 0
+                    len_i = len(string_text1)
+                    byte_count = 0
+                    while count < len_i and byte_count < text_len:
+                        if len(string_text1[count].encode('utf-8')) > 1:
+                            byte_count += 2
+                        else:
+                            byte_count += 1
+                        count += 1
+                    len_string = string_text1[:count]
+                    # len_string = string_text1[:text_len]
                 else:
-                    # count = 0
-                    # len_i = len(string_text2)
-                    # byte_count = 0
-                    # while count < len_i and byte_count < text_len:
-                    #     byte_count += len(string_text2[count])
-                    #     count += 1
+                    count = 0
+                    len_i = len(string_text2)
+                    byte_count = 0
+                    while count < len_i and byte_count < text_len:
+                        if len(string_text2[count].encode('utf-8')) > 1:
+                            byte_count += 2
+                        else:
+                            byte_count += 1
+                        count += 1
                     len_string = string_text2[:text_len]
             else:
-                len_string = string_text[:text_len]
+                count = 0
+                len_i = len(string_text)
+                byte_count = 0
+                while count < len_i and byte_count < text_len:
+                    if len(string_text[count].encode('utf-8')) > 1:
+                        byte_count += 2
+                    else:
+                        byte_count += 1
+                    count += 1
+                len_string = string_text[:count]
         # return len_string
-        return len_string.replace('-', '－').replace(' ', '　').replace('~', '～')
+        return len_string
+        # return len_string.replace('-', '－').replace(' ', '　').replace('~', '～')
 
     def limit_number_field(self, number=0.00, number_len=20, name=False):
         if name:
