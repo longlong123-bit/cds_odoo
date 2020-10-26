ea8d56b1464f843f88bc22ef8cf8edbfb61bef88
taideptroai <xuantai021997@gmail.com>
2020-10-26 10:16:57 +0700

    [FIX]Maintain_Quotations: update quotation calendar default value

--- custom/Maintain_Master_Price_List/models/master_price_list.py
+++ custom/Maintain_Master_Price_List/models/master_price_list.py
@@ -113,9 +113,7 @@ class ClassMasterPriceList(models.Model):
     price_applied = fields.Float(string="Price Applied")
 
     # 適用年月日
-    date_applied = fields.Date(string="Date Applied", default=datetime.now())
-
-    _sql_constraints = [('master.price.list', 'unique(maker_id, product_class_code_lv1_id, product_class_code_lv2_id, product_class_code_lv3_id, product_class_code_lv4_id, jan_code_id, product_code_id, country_state_code_id, supplier_group_code_id, country_state_code_id, industry_code_id, supplier_group_code_id, customer_code_bill_id, customer_code_id, recruitment_price_select, date_applied)', 'This data has been existed.')]
+    date_applied = fields.Date(string="Date Applied", default=datetime.today())
 
     # Listen event onchange maker_code (メーカーCD)
     @api.onchange('maker_id')
@@ -312,4 +310,6 @@ class ClassMasterPriceList(models.Model):
             self.customer_code = self.customer_code_id.customer_code
         else:
             self.customer_code = self.customer_name = ''
+
+    _sql_constraints = [('master.price.list', 'unique(maker_id, product_class_code_lv1_id, product_class_code_lv2_id, product_class_code_lv3_id, product_class_code_lv4_id, jan_code_id, product_code_id, country_state_code_id, supplier_group_code_id, country_state_code_id, industry_code_id, supplier_group_code_id, customer_code_bill_id, customer_code_id, recruitment_price_select, date_applied)', 'This data has been existed.')]
 
