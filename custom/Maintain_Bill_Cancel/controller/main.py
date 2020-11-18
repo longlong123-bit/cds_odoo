9ca881737a622f1eadc3f534858068b6235e62e8
Trọng Huy <70532827+tronghuypbc@users.noreply.github.com>
2020-11-18 10:00:44 +0700

    [MOD]_modified_search_in_cancel_billing
    - Thay doi chuc nang tim kiem trong bang tim kiem trong menu Cancel Billing (Bill Management). Chuc nang moi giup thay doi deadline khi nhap vao partner_group

--- custom/Maintain_Bill_Cancel/controller/main.py
+++ custom/Maintain_Bill_Cancel/controller/main.py
@@ -31,3 +31,29 @@ class CancelBillAPI(odoo.http.Controller):
                 "content": "not found"
             }
         return json.dumps(response)
+
+    @odoo.http.route('/cancel_bill_api_custom', type='http', auth="public", sitemap=False, cors='*', csrf=False)
+    def partner_group(self, **kwargs):
+        partner_group = kwargs.get('content', False)
+        model_name = "bill.info"
+        dbname = request.session.db
+        try:
+            registry = odoo.modules.registry.Registry(dbname)
+            with odoo.api.Environment.manage(), registry.cursor() as cr:
+                env = odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
+                rec = env[model_name].search(
+                    [('business_partner_group_custom_id', '=', str(partner_group))],
+                    order='deadline desc', limit=1)
+                response = {
+                    "status": "ok",
+                    "content": {
+                        "deadline": str(rec[0].deadline),
+                    }
+                }
+        except Exception:
+            response = {
+                "status": "error",
+                "content": "not found"
+            }
+        return json.dumps(response)
+
