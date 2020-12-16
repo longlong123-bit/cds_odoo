beb1281b9084455d6df6b5ec447491402702caf8
Trọng Huy <70532827+tronghuypbc@users.noreply.github.com>
2020-12-16 11:12:32 +0700

    [MOD]_cds_21_1284094118_update
    - https://vjppd.backlog.jp/view/CDS-21#comment-1284094118

--- custom/Quotation_Reports/models/normal_report.py
+++ custom/Quotation_Reports/models/normal_report.py
@@ -52,6 +52,72 @@ class PrintSale(models.Model):
                 subtotal += re.line_amount
         return subtotal
 
+    def limit_charater_field(self, string_text=None, text_len=20, name=False, first1=True):
+
+        text_len = text_len*2
+        count = 0
+        len_string = ''
+        COUNT_REPLACE = '〇'
+        if string_text:
+            # string_text = jaconv.h2z(string_text, kana=True, digit=True, ascii=True).replace('\uff0d', '-').replace('\xa0', ' ').replace('\uff5e', '~')
+            if name:
+                string_text1 = ''
+                string_text2 = ''
+                if len(string_text.splitlines()) - 1:
+                    string_text1 = string_text.splitlines()[0]
+                    string_text2 = string_text.splitlines()[1]
+                    string_text2_tmp = string_text2.replace('\uff0d', COUNT_REPLACE).replace('\xa0', COUNT_REPLACE).replace('\uff5e', COUNT_REPLACE)
+                else:
+                    string_text1 = string_text
+                    string_text2 = ''
+                string_text1_tmp = string_text1.replace('\uff0d', COUNT_REPLACE).replace('\xa0', COUNT_REPLACE).replace('\uff5e', COUNT_REPLACE)
+                if first1:
+                    len_i = len(string_text1)
+                    byte_count = 0
+                    while count < len_i and byte_count < text_len:
+                        try:
+                            if len(string_text1_tmp[count].encode('shift_jisx0213')) > 1 and byte_count < text_len - 1:
+                                byte_count += 2
+                            else:
+                                byte_count += 1
+                        except:
+                            byte_count += 2
+                        count += 1
+                    len_string = string_text1[:count]
+                    # len_string = string_text1[:text_len]
+                else:
+                    count = 0
+                    len_i = len(string_text2)
+                    byte_count = 0
+                    while count < len_i and byte_count < text_len:
+                        try:
+                            if len(string_text2_tmp[count].encode('shift_jisx0213')) > 1 and byte_count < text_len - 1:
+                                byte_count += 2
+                            else:
+                                byte_count += 1
+                        except:
+                            byte_count += 2
+                        count += 1
+                    len_string = string_text2[:count]
+            else:
+                string_text_tmp = string_text.replace('\uff0d', COUNT_REPLACE).replace('\xa0', COUNT_REPLACE).replace('\uff5e', COUNT_REPLACE)
+                count = 0
+                len_i = len(string_text)
+                byte_count = 0
+                while count < len_i and byte_count < text_len:
+                    try:
+                        if len(string_text_tmp[count].encode('shift_jisx0213')) > 1 and byte_count < text_len - 1:
+                            byte_count += 2
+                        else:
+                            byte_count += 1
+                    except:
+                        byte_count += 2
+                    count += 1
+                len_string = string_text[:count]
+        # return len_string
+        return len_string
+        # return len_string.replace('-', '－').replace(' ', '　').replace('~', '～')
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
