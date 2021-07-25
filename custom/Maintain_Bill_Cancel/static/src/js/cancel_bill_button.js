6fae8e3be0660d83b4603f4f89795b1480bb2ee4
Liem <liem@aipower.vn>
2021-07-25 15:42:43 +0700

    [deploy_jp]_deploy_jp_MERGE_FROM_VNTEST_TO_JPTEST

--- custom/Maintain_Bill_Cancel/static/src/js/cancel_bill_button.js
+++ custom/Maintain_Bill_Cancel/static/src/js/cancel_bill_button.js
@@ -1,9 +1,11 @@
 odoo.define('bill.cancel_bill_button', function (require) {
     "use strict";
-
+    var framework = require('web.framework');
     var ListController = require('web.ListController');
     var rpc = require('web.rpc');
     var session = require('web.session');
+    var core = require('web.core');
+    var _t = core._t;
     var includeDict = {
         renderButtons: function () {
             var self = this;
@@ -19,6 +21,7 @@ odoo.define('bill.cancel_bill_button', function (require) {
                 this.$buttons && this.$buttons.find('button.cancel_bill_button') && this.$buttons.find('button.cancel_bill_button').hide();
             } else {
                 this.$buttons.on('click', '.cancel_bill_button', function (e) {
+                    framework.blockUI();
                     const def = new $.Deferred();
                     var selected_data = [];
                     data = window.current_data || data;
@@ -29,6 +32,12 @@ odoo.define('bill.cancel_bill_button', function (require) {
                             }
                         }
                     }
+
+                    if (selected_data.length == 0) {
+                        framework.unblockUI();
+                        return def;
+                    }
+
                     rpc.query({
                         model: model,
                         method: 'cancel_bill_for_invoice',
@@ -37,11 +46,16 @@ odoo.define('bill.cancel_bill_button', function (require) {
                             context: JSON.stringify(session.user_context),
                         }
                     }, {
-                        timeout: 3000,
+                        timeout: 6000000,
                         shadow: true
                     }).then(function (result) {
                         if (result) {
+                            framework.unblockUI();
+                            self.do_notify('Infomation','請求処理の取消が完了しました。',false);
                             return self.do_action(result)
+                        }else{
+                            framework.unblockUI();
+                            self.do_warn('エラー',"最新締切がある請求処理のみを取消できます。",false);
                         }
                     })
                     return def;
